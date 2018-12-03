86a85eeeaabeea7c8d2630d3781fb128249fb4fd
Domonic Milesi <dom-o@users.noreply.github.com>
2018-12-03 00:21:40 -0500

    Add schema for Dark Sky datapoint.

--- client/templates/create.js
+++ client/templates/create.js
@@ -7,7 +7,9 @@ module.exports = function() {
 				I like to go <input id="activityname" name="activityname" type="text">
 				at <input id="lat" name="lat" type="text"> <input id="lng" name="lng" type="text"> 
 				when <input id="weathercondition" name="weathercondition" type="text">...
+				<button>more conditions</button>
 			</form>
 		</div>
 	`
 }
+
