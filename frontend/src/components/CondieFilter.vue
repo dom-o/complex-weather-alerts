12ad27870c3d75ac567a66481dd031f3988a2d86
Domonic Milesi <dom-o@users.noreply.github.com>
2019-03-07 02:06:04 -0500

    Finish most of initial form.

---
+++ frontend/src/components/CondieFilter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <select v-model="selected">
+      <option v-for="(option, optionName) in condieFilter.options" :key="optionName" :value="option.description">
+        {{ option.title }}
+      </option>
+    </select>
+    {{ selected }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CondieFilter',
+  props: {
+    condieFilter: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selected:'',
+    }
+  },
+}
+</script>
--- frontend/src/components/Create.vue
+++ frontend/src/components/Create.vue
@@ -3,17 +3,39 @@
   <form>
     I like to go <input id="activityname" name="activityname" type="text">
     at <input id="lat" name="lat" type="text"> <input id="lon" name="lon" type="text">
-    when the <ConditionSelect/> <ConditionFilter/>
-    <button>more conditions</button>
+    when the <CondieFilterContainer/>
+    I'm free
+      <input type="checkbox" id="mondaySelect" name="daysFree"><label for="mondaySelect">Monday</label>
+      <input type="checkbox" id="tuesdaySelect" name="daysFree"><label for="tuesdaySelect">Tuesday</label>
+      <input type="checkbox" id="wednesdaySelect" name="daysFree"><label for="wednesdaySelect">Wednesday</label>
+      <input type="checkbox" id="thursdaySelect" name="daysFree"><label for="thursdaySelect">Thursday</label>
+      <input type="checkbox" id="fridaySelect" name="daysFree"><label for="fridaySelect">Friday</label>
+      <input type="checkbox" id="saturdaySelect" name="daysFree"><label for="saturdaySelect">Saturday</label>
+      <input type="checkbox" id="sundaySelect" name="daysFree"><label for="sundaySelect">Sunday</label>
+    I wanna know about good condies
+      <select id="advanceNotice">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+      </select>
+    days in advance
+    Reach me at <input type="email" id="email" required>
+    <input type="submit" value="Go">
   </form>
 </div>
 </template>
 
 <script>
-import ConditionSelect from './ConditionSelect.vue'
+import CondieFilterContainer from './CondieFilterContainer.vue'
 
 export default {
-  components: {ConditionSelect},
+  components: {CondieFilterContainer},
+  methods: {
+  },
   name: 'Create'
 }
 </script>
