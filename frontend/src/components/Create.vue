4c6c3f95d67877ca6bef3ce2a025fd1d06c347ed
Domonic Milesi <dom-o@users.noreply.github.com>
2019-03-19 18:58:11 -0400

    Add select all option for days available.

--- frontend/src/components/Create.vue
+++ frontend/src/components/Create.vue
@@ -2,16 +2,17 @@
 <div>
   <form>
     I like to go <input id="activityname" name="activityname" type="text">
-    at <input id="lat" name="lat" type="text"> <input id="lon" name="lon" type="text">
+    at <input id="lat" name="lat" type="text" required> <input id="lon" name="lon" type="text" required>
     when the <CondieFilterContainer/>
     I'm free
-      <input type="checkbox" id="mondaySelect" name="daysFree"><label for="mondaySelect">Monday</label>
-      <input type="checkbox" id="tuesdaySelect" name="daysFree"><label for="tuesdaySelect">Tuesday</label>
-      <input type="checkbox" id="wednesdaySelect" name="daysFree"><label for="wednesdaySelect">Wednesday</label>
-      <input type="checkbox" id="thursdaySelect" name="daysFree"><label for="thursdaySelect">Thursday</label>
-      <input type="checkbox" id="fridaySelect" name="daysFree"><label for="fridaySelect">Friday</label>
-      <input type="checkbox" id="saturdaySelect" name="daysFree"><label for="saturdaySelect">Saturday</label>
-      <input type="checkbox" id="sundaySelect" name="daysFree"><label for="sundaySelect">Sunday</label>
+      <input type="checkbox" id="anyday" v-model="allSelected"><label for="anyday">Anytime</label>
+      <input type="checkbox" id="monday" value="Monday" v-model="days"><label for="monday">Monday</label>
+      <input type="checkbox" id="tuesday" value="Tuesday" v-model="days"><label for="tuesday">Tuesday</label>
+      <input type="checkbox" id="wednesday" value="Wednesday" v-model="days"><label for="wednesday">Wednesday</label>
+      <input type="checkbox" id="thursday" value="Thursday" v-model="days"><label for="thursday">Thursday</label>
+      <input type="checkbox" id="friday" value="Friday" v-model="days"><label for="friday">Friday</label>
+      <input type="checkbox" id="saturday" value="Saturday" v-model="days"><label for="saturday">Saturday</label>
+      <input type="checkbox" id="sunday" value="Sunday" v-model="days"><label for="sunday">Sunday</label>
     I wanna know about good condies
       <select id="advanceNotice">
         <option value="1">1</option>
@@ -34,7 +35,19 @@ import CondieFilterContainer from './CondieFilterContainer.vue'
 
 export default {
   components: {CondieFilterContainer},
-  methods: {
+  watch: {
+    allSelected: function(val) {
+      if(val) {
+        this.days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+      }
+    }
+  },
+  methods: {},
+  data () {
+    return {
+      allSelected: false,
+      days: []
+    }
   },
   name: 'Create'
 }
